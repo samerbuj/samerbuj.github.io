7983a86155230fd6320e12eb76be9d47fb054475
Samer Bujana Escalona <68027116+samerbuj@users.noreply.github.com>
2026-04-15 12:41:08 +0200

    fixed something with the config data

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
-const CONFIG = {
+window.CONFIG = {
     GOOGLE_MAPS_API_KEY: "AIzaSyAxF46uw7iBJkVyUtMZ6oIyim5x1YlLLWc",
-
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
+    
+    FIREBASE_CONFIG = {
         apiKey: "AIzaSyC5-Em_g4joTwydCiekxZ_rMTKYo4RNfKw",
         authDomain: "bmo-tracker-520bc.firebaseapp.com",
         projectId: "bmo-tracker-520bc",
         storageBucket: "bmo-tracker-520bc.firebasestorage.app",
         messagingSenderId: "244981410984",
         appId: "1:244981410984:web:d0d1d90a09359b3b74269d"
-    };
+    }
 };
